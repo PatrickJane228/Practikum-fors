c6556b31b58f790144f07bacc5a844a6e13cb710
Ilya <123509915+PatrickJane228@users.noreply.github.com>
2023-02-22 10:45:58 +0200

    Add files via upload

---
+++ 113/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            byte n = byte.Parse(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"array [{i}] =");
+                array.SetValue(int.Parse(Console.ReadLine()), i);
+            }
+
+                
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        Console.WriteLine($"{array.GetValue(i)}");
+                    }
+            Console.ReadKey();   
+            
+        }
+    }
+}
